432817730c2d420c1a9396fbd8b9b75a6c788176
Nathan John <nafanjohn@gmail.com>
2023-06-29 12:03:57 +0100

    updated basket code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Basket from 'basketModal';
 
+// the below needs to be on the adopt page/section and then passed to the Basket function
+const [selectedCats, setSelectedCats] = useState([
+  catName: "", catBreed: "", catImgSrc: ""
+]);
+
+
 function App() {
   return (
     <div className="App">
       <h1>
         Cats 4 Life - Coming Soon!
       </h1>
-      <Basket />
+      <Basket selectedCats={selectedCats} />
       <p>
         Created by Chris Kelly, Chloe Moss, Idnan Hussain and Nathan Hynes
       </p>
